6d747e8da0a3113499457debb919e11cd06037c0
Denis Michailov <kamrad.reg@gmail.com>
2016-11-15 22:02:52 +1000

    everything can be moved

--- app/js/components/work.js
+++ app/js/components/work.js
@@ -2,18 +2,24 @@
 
 var $ = jQuery = require('jquery');
 
+var maxLeft = 0;
+var maxTop = 0;
+
 module.exports = (function() {
 
 	// cache dom
 	var $canva = $(".canva");
+	var $elements = $(".element");
 	var rect = document.getElementById('canva').getBoundingClientRect()
-	console.log(rect);
 
-	$('.element').each((index, element)=>{
+	$elements.each((index, element)=>{
 		var elementWidth = 100;
 		var elementHeight = 100;
-		var maxLeft = $canva.width() - elementWidth;
-		var maxTop = $canva.height() - elementHeight;
+		console.dir($canva);
+		maxLeft = $canva[0].clientWidth - elementWidth;
+		maxTop = $canva[0].clientHeight - elementHeight;
+		$(".maxLeft").find("span").text(maxLeft);
+		$(".maxTop").find("span").text(maxTop);
 		var left = getRand(1, maxLeft);
 		var top = getRand(1, maxTop);
 		$(element).css('left', left);
@@ -21,10 +27,32 @@ module.exports = (function() {
 		$(element).css('background-color', getRandColor());
 	});
 
-	$canva.on('click', function(e) {
-		console.dir(e.clientX);
-		console.dir(e.clientY);
-		console.dir($canva);
+	$elements.on('mousedown', function(e) {
+
+		var $el = $(this);
+		moveTo(e);
+		$canva.append($el);
+		$el.css("z-index", 100);
+
+		$canva.on("mousemove", moveTo);
+		$canva.on("mouseup", unbindAll);
+
+		function moveTo (e) {
+			var left = e.pageX - $canva[0].offsetLeft - 50;
+			var top = e.pageY - $canva[0].offsetTop -  50;
+			// console.log(`${left}, ${top}`);
+			if((left > -1) && (top > -1) && (left < maxLeft) && (top < maxTop) ) {
+				$el.css("left", left);
+				$el.css("top", top);
+				console.log(left, top);
+			}
+		};
+
+		function unbindAll() {
+			$canva.off("mousemove");
+			$el.off("mouseup");
+		};
+
 	});
 
 	return {
@@ -32,7 +60,6 @@ module.exports = (function() {
 	};
 
 })();
-
 
 // FUNCTIONALITY
 function getRandColor() {
