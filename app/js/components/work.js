0b53a4dc41a31d50f889becb5facb115c01a3984
Denis Michailov <kamrad.reg@gmail.com>
2016-11-15 23:57:53 +1000

    rewrite on clean js. add real border

--- app/js/components/work.js
+++ app/js/components/work.js
@@ -30,40 +30,62 @@ module.exports = (function() {
 	}
 
 	$canva.addEventListener('mousedown', function(e) {
+		var target;
 		if (e.target.className == 'element') {
+			target = e.target;
 			moveTo(e);
+		} else {
+			return 0;
 		}
-		$canva.appendChild(e.target);
-		$canva.addEventListener('mousemove', moveTo, false)
-		$canva.addEventListener('mouseup', unbindAll);
+		$canva.appendChild(target);
+		document.addEventListener('mousemove', moveTo, false)
+		document.addEventListener('mouseup', unbindAll);
+
+		function unbindAll () {
+			document.removeEventListener('mousemove', moveTo);
+			document.removeEventListener('mouseup', unbindAll);
+		}
 
 		function moveTo (e) {
 			var left = e.pageX - $canva.offsetLeft - 50;
 			var top = e.pageY - $canva.offsetTop -  50;
-			if((left > -1) && (top > -1) && (left < maxLeft) && (top < maxTop) ) {
-				e.target.style.left = left + 'px';
-				e.target.style.top = top + 'px';
+			if((left < -1) && (top > -1) && (top < maxTop)) {
+				target.style.left = '0px';
+				target.style.top = top + 'px';
+			} else if ((top < -1) && (left < maxLeft) && (left > -1))  {
+				target.style.left = left + 'px';
+				target.style.top = '0px';
+			} else if ((left > maxLeft) && (top > -1) && (top < maxTop)) {
+				target.style.left = maxLeft + 'px';
+				target.style.top = top + 'px';
+			} else if ((top > maxTop) && (left > -1) && (left < maxLeft)) {
+				target.style.left = left + 'px';
+				target.style.top = maxTop + 'px';
+			} else if ((left < -1) && (top < -1)) {
+				target.style.left = '0px';
+				target.style.top = '0px';
+			} else if ((left > maxLeft) && (top > maxTop)) {
+				target.style.left = maxLeft + 'px';
+				target.style.top = maxTop + 'px';
+			} else if ((top < -1) && (left > maxLeft)) {
+				target.style.left = maxLeft + 'px';
+				target.style.top = '0px';
+			} else if ((left < -1) && (top > maxTop)) {
+				target.style.left = '0px';
+				target.style.top = maxTop + 'px';
+			} else {
+				target.style.left = left + 'px';
+				target.style.top = top + 'px';
 			}
 		};
 	}, false);
 
 })();
-// 		function unbindAll() {
-// 			$canva.off("mousemove");
-// 			$el.off("mouseup");
-// 		};
-//
-// 	});
-//
-// 	return {
-// 		test: 'test'
-// 	};
-//
-// })();
 
 // FUNCTIONALITY
 function getRandColor() {
-	return "rgba(" + getRand(0, 255) + "," + getRand(0, 255) + "," + getRand(0, 255) + "," + 1/getRand(1, 10) + ")"
+	var opacity = (getRand(30, 100)*0.01).toString();
+	return "rgba(" + getRand(0, 255) + "," + getRand(0, 255) + "," + getRand(0, 255) + "," + opacity + ")"
 }
 
 function getRand(min, max) {
